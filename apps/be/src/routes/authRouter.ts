67c32c5cecf21a9db1611068540cf6710ace8c8f
Kartik Tomar <kartiktomar112233@gmail.com>
2025-08-31 11:43:02 +0530

    inital balance of 5000usdt added

--- apps/be/src/routes/authRouter.ts
+++ apps/be/src/routes/authRouter.ts
@@ -15,76 +15,87 @@ authRouter.post("/signup", async (req: Request, res: Response) => {
       email: string;
       password: string;
       confirmPassword: string;
-    }
+    };
 
     if (!email || !password || !confirmPassword) {
-      return res.status(400).json({ message: "All fields are required"});
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords do not match"});
+      return res.status(400).json({ message: "Passwords do not match" });
     }
 
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
 
-    if (existingUser){
+    if (existingUser) {
       return res.status(403).json({ message: "Error while signing up" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-      },
+    const user = await prisma.$transaction(async (tx) => {
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      await tx.balance.create({
+        data: {
+          userId: newUser.id,
+          assetId: "USDT",
+          qtyInt: BigInt(500000),
+        },
+      });
+
+      return newUser;
     });
 
     return res.status(200).json({
-      message: "User created successfully",
-      user: { userId: user.id },
+      userId: user.id,
     });
-
   } catch (err: any) {
-    console.error(err);
+    console.error("signup error", err);
     return res.status(403).json({ message: "Error while signing up" });
   }
-  });
+});
 
-  authRouter.post("/signin", async (req: Request, res: Response) => {
-    try {
-      const { email, password } = req.body as { email: string, password: string};
+authRouter.post("/signin", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body as { email: string; password: string };
 
-      if (!email || !password) {
-        return res.status(400).json({ message: "Email and passwords are required "})
-      }
-
-      const user = await prisma.user.findUnique({ where: { email } });
-      if (!user) {
-        res.status(403).json({ message: "Incorrect credentials" });
-        return;
-      }
-  
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        res.status(400).json({ message: "Invalid password" });
-        return;
-      }
-
-      const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
-        expiresIn: "1h",
-      });
-  
-      res.status(200).json({
-        message: "Signin successful",
-        token,
-        user: { id: user.id, email: user.email },
-      });
-
-    } catch(err: any) {
-      console.error("Signin error:", err);
-      return res.status(500).json({ message: "Internal Server Error" });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and passwords are required " });
     }
-})
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      res.status(403).json({ message: "Incorrect credentials" });
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      res.status(400).json({ message: "Invalid password" });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      message: "Signin successful",
+      token,
+      user: { id: user.id, email: user.email },
+    });
+  } catch (err: any) {
+    console.error("Signin error:", err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
