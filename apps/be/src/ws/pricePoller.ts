0e7c842f0cf4bfda66f1e69ee4184b002abcbb98
Kartik Tomar <kartiktomar112233@gmail.com>
2025-08-31 12:11:32 +0530

    added polygonWS for tsla trade

--- apps/be/src/ws/pricePoller.ts
+++ apps/be/src/ws/pricePoller.ts
@@ -4,48 +4,113 @@ import { Kafka } from "kafkajs";
 const kafka = new Kafka({ clientId: "tick-producer", brokers: ["localhost:9092"] });
 const producer = kafka.producer();
 
-const symbols = ["btcusdt", "ethusdt", "solusdt"];
-const streams = symbols.map((s) => `${s}@aggTrade`).join("/");
-const binanceWS = new WebSocket(`wss://stream.binance.com:9443/stream?streams=${streams}`);
+const binanceSymbols = ["btcusdt", "solusdt"];
+const binanceStreams = binanceSymbols.map((s) => `${s}@trade`).join("/");
+const BINANCE_URL = `wss://stream.binance.com:9443/stream?streams=${binanceStreams}`;
 
-async function start() {
-  await producer.connect();
-  console.log("Kafka Producer connected");
+const POLYGON_URL = `wss://socket.polygon.io/stocks`;
+const POLYGON_KEY = process.env.POLYGON_KEY!;
+const equitySymbols = ["TSLA"];
+
+async function publishTick(tick: any) {
+  try {
+    await producer.send({
+      topic: "ticks",
+      messages: [{ key: tick.assetId, value: JSON.stringify(tick) }],
+    });
+  } catch (err: any) {
+    console.error("Kafka publish error", err);
+  }
+}
+
+function startBinanceWS() {
+  const binanceWS = new WebSocket(BINANCE_URL);
 
   binanceWS.on("open", () => {
-    console.log(`Connected to Binance WS (${symbols.join(", ")})`);
-  });
+    console.log(`Connected to Binance WS (${binanceSymbols.join(", ")})`);
+  })
 
-  binanceWS.on("message", async (msg) => {
+  binanceWS.on("message", async(msg) => {
     try {
       const parsed = JSON.parse(msg.toString());
       const data = parsed.data;
       const streamSymbol = parsed.stream.split("@")[0].toUpperCase();
 
       const tick = {
-        ts: new Date(data.T).toISOString(), 
+        ts: new Date(data.T).toISOString(),
         assetId: streamSymbol,
-        price: parseFloat(data.p),          
+        source: "BINANCE",
+        price: parseFloat(data.p),
       };
 
-      await producer.send({
-        topic: "ticks",
-        messages: [{ key: tick.assetId, value: JSON.stringify(tick) }],
-      });
-    } catch (err) {
-      console.error("Error parsing Binance tick:", err);
+      await publishTick(tick);
+    } catch(err: any) {
+      console.error("Error parsing binance tick:", err);
     }
   });
 
   binanceWS.on("close", () => {
-    console.error("Binance WS closed. Reconnecting in 5s...");
-    setTimeout(start, 5000);
+    console.error("Binance WS Closed. Reconnecting in 5s..");
+    setTimeout(startBinanceWS, 5000);
   });
 
   binanceWS.on("error", (err) => {
-    console.error("Binance WS error:", err);
+    console.error("Eror connecting to binanceWS", err);
     binanceWS.close();
   });
+}
+
+function startPolygonWS() {
+  const polygonWS = new WebSocket(POLYGON_URL);
+
+  polygonWS.on("open", () => {
+    console.log("Connected to polygon WS");
+    polygonWS.send(JSON.stringify({ action: "auth", params: POLYGON_KEY } ));
+    polygonWS.send(
+      JSON.stringify({
+        action: "subscribe",
+        params: equitySymbols.map((s) => `T.${s}`).join(","),
+      })
+    );
+  });
+
+  polygonWS.on("message", async(msg) => {
+    try {
+      const updates = JSON.parse(msg.toString());
+      for (const u of updates) {
+        if (u.ev === "T") {
+          const tick = {
+            ts: new Date(u.t).toISOString(),
+            assetId: u.sym,
+            source: "POLYGON",
+            price: u.p,
+          };
+
+          await publishTick(tick);
+        }
+      }
+    } catch(err: any) {
+      console.error("Error parsing Polygon tick:", err);
+    }
+  });
+
+  polygonWS.on("close", () => {
+    console.error("Polygon WS closed, Reconnecting in 5s...");
+    setTimeout(startPolygonWS, 5000);
+  });
+
+  polygonWS.on("error", (err) => {
+    console.error("Polygon WS error:", err);
+    polygonWS.close();
+  });
+}
+
+async function start() {
+  await producer.connect();
+  console.log("Kafka Producer connected");
+
+  startBinanceWS();
+  startPolygonWS();
 }
 
 start().catch(console.error);
